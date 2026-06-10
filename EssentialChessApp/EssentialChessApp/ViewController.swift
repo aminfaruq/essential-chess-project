4908003c0bfbdfb57aceca029d6fdf151569ff0e
Amin Faruq <aminfaruk.fa@gmail.com>
2026-06-10 12:05:59 +0800

    create new essential chess app

---
+++ EssentialChessApp/EssentialChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialChessApp
+//
+//  Created by Amin faruq on 10/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
