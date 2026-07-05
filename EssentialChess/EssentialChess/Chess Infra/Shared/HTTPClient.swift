f9d3f64814cced50a5f8d6abb562155f4a6b697b
Amin Faruq <aminfaruk.fa@gmail.com>
2026-07-05 15:30:51 +0800

    test(cloud-eval):  RemoteCloudEvaluation

--- EssentialChess/EssentialChess/Chess Infra/Shared/HTTPClient.swift
+++ EssentialChess/EssentialChess/Chess Infra/Shared/HTTPClient.swift
@@ -10,5 +10,11 @@ public protocol HTTPClient {
     
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
-    func get(from url: URL, completion: @escaping (Result) -> Void)
+    func get(from url: URL, headers: [String: String]?, completion: @escaping (Result) -> Void)
+}
+
+public extension HTTPClient {
+    func get(from url: URL, completion: @escaping (Result) -> Void) {
+        get(from: url, headers: nil, completion: completion)
+    }
 }
